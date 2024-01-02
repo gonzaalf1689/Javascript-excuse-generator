7a04347564d0481f046a4e35299df01b8ce2c177
Alfredo <106854439+gonzaalf1689@users.noreply.github.com>
2024-01-02 01:40:54 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
 };
+
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "The racoon", "The neighbor"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car", "my front door"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished work",
+    "during my lunch",
+    "while I was partying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`;
+}
